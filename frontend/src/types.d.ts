ee70ee8d0c845ecfc6538af0ffcbf12d6bb24d9f
Koretada <koretadaa@gmail.com>
2026-04-07 12:13:21 +0200

    fix: ajout du type  material/Chip

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -5,3 +5,12 @@ declare module '*.css' {
 }
 
 declare module 'xterm/css/xterm.css'
+
+// MUI Component Augmentations
+import '@mui/material/Chip'
+
+declare module '@mui/material/Chip' {
+  interface ChipPropsVariantOverrides {
+    tonal: true
+  }
+}
